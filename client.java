17abc5ef03cb59dfdbf0890d427773f9e60f2080
jacobvanderwyst <vjake870@gmail.com>
2022-10-14 21:21:07 -0500

    modified:   client.java 	modified:   server.java

--- client.java
+++ client.java
@@ -1,53 +1,53 @@
 
-// client
-import java.net.*;
-import java.util.Scanner;
+// Client2 class that
+// sends data and receives also
+  
 import java.io.*;
-
-public class client {
-    private Socket socket = null; //client instance
-    private OutputStreamWriter cliSockWrite=null;
-    BufferedReader clisockreadWrapper=null;
-    private Scanner input= new Scanner(System.in);
-    
-    public client(String address, int port) throws IOException{
-        try {
-            socket = new Socket(address, port); 
-            System.out.println("conncting to " + address + ":" + port);
-            cliSockWrite=(new OutputStreamWriter(socket.getOutputStream(),"UTF8"));
-            clisockreadWrapper=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF8"));
-            
-            System.out.println("Say something... stop to end connection");
-            String mToServe=input.nextLine();
-            //System.out.println((mToServe));
-            while((mToServe.equals("stop"))== false){
-                System.out.println("Say something... stop to close connection");
-                mToServe=input.nextLine();
-                try{
-                    cliSockWrite.write(mToServe+"\r\n");
-                    cliSockWrite.flush();
-                    //cliSockWrite.close();
-                    if(mToServe.equals("stop")){
-                        System.out.println("breaking");
-                        System.exit(0);
-                    }
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                System.out.println(mToServe);
-            }
-        } catch (IOException e) {
-            System.out.println(e);
-            cliSockWrite.close();
-            socket.close();
+import java.net.*;
+  
+class client {
+  
+    public static void main(String args[])
+        throws Exception
+    {
+  
+        // Create client socket
+        Socket s = new Socket("44.208.139.146", 4000);
+  
+        // to send data to the server
+        DataOutputStream dos
+            = new DataOutputStream(
+                s.getOutputStream());
+  
+        // to read data coming from the server
+        BufferedReader br
+            = new BufferedReader(
+                new InputStreamReader(
+                    s.getInputStream()));
+  
+        // to read data from the keyboard
+        BufferedReader kb
+            = new BufferedReader(
+                new InputStreamReader(System.in));
+        String str, str1;
+  
+        // repeat as long as exit
+        // is not typed at client
+        while (!(str = kb.readLine()).equals("exit")) {
+  
+            // send to the server
+            dos.writeBytes(str + "\n");
+  
+            // receive from the server
+            str1 = br.readLine();
+  
+            System.out.println(str1);
         }
-        
-    }
-
-    public static void main(String args[])throws IOException {
-       
-            client cl= new client("44.208.139.146", 4000);
-        
+  
+        // close connection.
+        dos.close();
+        br.close();
+        kb.close();
+        s.close();
     }
 }
--- server.java
+++ server.java
@@ -1,49 +1,64 @@
+// Server2 class that
+// receives data and sends data
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
-public class server {
-    private ServerSocket middleman;
-    private int port = 4000;
-    private Socket client;
-    private InputStreamReader cliSockReader=null;
-    private OutputStreamWriter cliSockWrite=null;
-    private BufferedReader clisockreadWrapper=null;
-    private Scanner inp=null;
-    protected void createSocketServer()
-    {
-        try
-        {
-            while (true){
-                System.out.println("Creating socket server on port: "+port+" ...");
-                middleman = new ServerSocket(port);
-                
-                client = middleman.accept();
-                cliSockWrite=(new OutputStreamWriter(client.getOutputStream(),"UTF8"));
-                System.out.println("Accepted connection from "+client.getInetAddress().getHostAddress());
-                middleman.close();
-                inp= new Scanner(System.in);
-                cliSockReader=(new InputStreamReader(client.getInputStream(),"UTF8"));
-                clisockreadWrapper=new BufferedReader(new InputStreamReader(client.getInputStream(),"UTF8"));
-                String line;
-                while((line = clisockreadWrapper.readLine()) != null)
-                {
-                    System.out.println("Client: "+line);
-                    if((line.equals("stop"))==true){
-                        System.exit(0);
-                    }
-                }
-            }
-        }
-        catch(IOException e)
-        {
-            System.out.println(e);
-        }
-    }
+class server {
 
-    public static void main(String[] args){
-        server test = new server();
-        test.createSocketServer();
-    }
+	public static void main(String args[])
+		throws Exception
+	{
+
+		// Create server Socket
+		ServerSocket ss = new ServerSocket(4000);
+
+		// connect it to client socket
+		Socket s = ss.accept();
+		System.out.println("Connection established");
+
+		// to send data to the client
+		PrintStream ps
+			= new PrintStream(s.getOutputStream());
+
+		// to read data coming from the client
+		BufferedReader br
+			= new BufferedReader(
+				new InputStreamReader(
+					s.getInputStream()));
+
+		// to read data from the keyboard
+		BufferedReader kb
+			= new BufferedReader(
+				new InputStreamReader(System.in));
+
+		// server executes continuously
+		while (true) {
+
+			String str, str1;
+
+			// repeat as long as the client
+			// does not send a null string
+
+			// read from client
+			while ((str = br.readLine()) != null) {
+				System.out.println(str);
+				str1 = kb.readLine();
+
+				// send to client
+				ps.println(str1);
+			}
+
+			// close connection
+			ps.close();
+			br.close();
+			kb.close();
+			ss.close();
+			s.close();
+
+			// terminate application
+			System.exit(0);
+
+		} // end of while
+	}
 }
