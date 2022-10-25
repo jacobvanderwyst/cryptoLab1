4a138ead7878d9e5d8a7eac37996f80c3507f815
jacobvanderwyst <vjake870@gmail.com>
2022-10-25 12:43:44 -0500

    comp

--- compile.py
+++ compile.py
@@ -6,7 +6,7 @@ os.system("sudo rm -rf /var/www/html/cryptoLab1")
 os.system("sudo rm -f {path}sources.txt")
 os.system("echo \"removed old\"")
 os.system("cd {path} && git clone https://github.com/jacobvanderwyst/cryptoLab1.git")
-os.system("cd /var/www/html/cryptoLab1 && find -name \"*.java\" > sources.txt")
+os.system("find -name \"*.java\" > sources.txt")
 os.system("javac @sources.txt")
 os.system("echo \"compiled classes\"")
 
