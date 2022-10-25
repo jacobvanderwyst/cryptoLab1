faae6b3e9a2c1824f38cb56cf2b4e79fe9440638
jacobvanderwyst <vjake870@gmail.com>
2022-10-25 11:15:32 -0500

    compile

--- compile.py
+++ compile.py
@@ -4,10 +4,9 @@ path="/var/www/html/"
 filename="server"
 os.system("sudo rm -rf /var/www/html/cryptoLab1")
 os.system("cd /var/www/html && git clone https://github.com/jacobvanderwyst/cryptoLab1.git")
-os.system(f"javac -d . {path}cryptoLab1/{filename}.java")
-os.system(f"jar cvf {path}cryptoLab1/{filename}.jar {path}cryptoLab1/{filename}.class")
+os.system(f"javac -d {path}cryptoLab1 {path}cryptoLab1/{filename}.java")
 os.system(f"echo \"Main-Class: {filename}\" > MANIFEST.MF")
-os.system(f"jar cvmf MANIFEST.MF {filename}.jar {filename}.class")
+os.system(f"jar cvmf MANIFEST.MF {filename}.jar {path}cryptoLab1/*.class")
 os.system(f"sudo chmod 777 {filename}.jar")
 os.system("tree")
-os.system(f"sudo mv /var/www/html/cryptoLab1/{filename}.jar /var/www/html/")
+#os.system(f"sudo mv /var/www/html/cryptoLab1/{filename}.jar /var/www/html/")
