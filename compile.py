15a39111c1b0fbc15ebbf599dfb517b38ea38549
jacobvanderwyst <vjake870@gmail.com>
2022-10-25 11:54:56 -0500

    compile

--- compile.py
+++ compile.py
@@ -10,7 +10,7 @@ os.system("cd /var/www/html && git clone https://github.com/jacobvanderwyst/cryp
 os.system("find -name \"*.java\" > sources.txt")
 os.system(f"javac @sources.txt")
 os.system(f"echo \"Main-Class: {filename}\" > MANIFEST.MF")
-os.system(f"jar cvmf MANIFEST.MF {filename}.jar makeServerSockets.class server.class")
+os.system(f"jar cvmf MANIFEST.MF {filename}.jar {path}cryptoLab1/makeServerSockets.class {path}cryptoLab1/server.class")
 os.system(f"sudo chmod 777 {filename}.jar")
 os.system("tree")
 #os.system(f"sudo mv /var/www/html/cryptoLab1/{filename}.jar /var/www/html/")
