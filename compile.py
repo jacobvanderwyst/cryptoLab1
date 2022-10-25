7b0bdd6c3df8f403845231ecd81f2e804effb197
jacobvanderwyst <vjake870@gmail.com>
2022-10-25 12:47:42 -0500

    c

--- compile.py
+++ compile.py
@@ -3,9 +3,7 @@ import os
 path="/var/www/html/"
 filename="server"
 os.system("sudo rm -rf /var/www/html/cryptoLab1")
-os.system("sudo rm -f {path}sources.txt")
-os.system("echo \"removed old\"")
-os.system("cd {path} && git clone https://github.com/jacobvanderwyst/cryptoLab1.git")
+os.system("cd /var/www/html && git clone https://github.com/jacobvanderwyst/cryptoLab1.git")
 os.system("find -name \"*.java\" > sources.txt")
 os.system("javac @sources.txt")
 os.system("echo \"compiled classes\"")
